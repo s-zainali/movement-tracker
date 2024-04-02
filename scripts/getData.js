767579f6659d03507c8271dfb47b7fe9ae092efb
Syed Zain Ali <szainali284@gmail.com>
2024-04-02 02:29:55 -0700

    added velocity button

--- scripts/getData.js
+++ scripts/getData.js
@@ -1,5 +1,4 @@
 
-
 function handleOrientation(event) {
   updateFieldIfNotNull('Orientation_a', event.alpha);
   updateFieldIfNotNull('Orientation_b', event.beta);
@@ -8,14 +7,25 @@ function handleOrientation(event) {
 }
 
 function incrementEventCount(){
-  let counterElement = document.getElementById("stats")
+  let counterElement = document.getElementById("num-observed-events")
   let eventCount = parseInt(counterElement.innerHTML)
   counterElement.innerHTML = eventCount + 1;
 }
 
-function updateFieldIfNotNull(fieldName, value, precision=10){
+function updateFieldIfNotNull(fieldName, value, precision=2){
   if (value != null)
     document.getElementById(fieldName).innerHTML = value.toFixed(precision);
+}
+
+let accelerations = {
+  x: [],
+  y: [],
+  z: []
+}
+
+
+function computeMaxVelocity() {
+  console.log(accelerations);
 }
 
 function handleMotion(event) {
@@ -26,6 +36,10 @@ function handleMotion(event) {
   updateFieldIfNotNull('Accelerometer_x', event.acceleration.x);
   updateFieldIfNotNull('Accelerometer_y', event.acceleration.y);
   updateFieldIfNotNull('Accelerometer_z', event.acceleration.z);
+
+  accelerations.x.push(event.acceleration.x);
+  accelerations.y.push(event.acceleration.y);
+  accelerations.z.push(event.acceleration.z);
 
   updateFieldIfNotNull('Accelerometer_i', event.interval, 2);
 
@@ -83,4 +97,3 @@ window.addEventListener('userproximity', function(event) {
    document.getElementById("UserProximity").innerHTML="UserProximity: "+event.near;
 });
 */
-
